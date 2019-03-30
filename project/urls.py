cbd41aa65518a4dbcfc51b41f2dde00b08f85d48
dylan-meiners <dylanmeiners@gmail.com>
2019-03-30 14:22:24 -0500

    urls debugging

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('scraper.urls')),
     path('scraper/', include('scraper.urls')),
     path('admin/', admin.site.urls),
 ]
