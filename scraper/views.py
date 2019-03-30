18de01b66e61f24dd89a4a3c12259dadf7783120
dylan-meiners <dylanmeiners@gmail.com>
2019-03-30 15:03:00 -0500

    imported templates

--- scraper/views.py
+++ scraper/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from templates import *
+from ./templates import *
 
 def index(request):
     return render(request, 'index.html')
