5cd45f349520e003199ed24167d0f3e43cc8ef02
dylan-meiners <dylanmeiners@gmail.com>
2019-04-01 17:17:22 -0500

    url debugging

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'project/scraper/templates',
+            'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
