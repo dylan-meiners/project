b6daa86327d1d49b8a2ab6efb3fda23b924d6e03
dylan-meiners <dylanmeiners@gmail.com>
2019-04-13 06:38:00 -0500

    Fixing static stuff

--- project/settings.py
+++ project/settings.py
@@ -128,7 +128,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR
 STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = [
