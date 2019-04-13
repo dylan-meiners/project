f29834209cffca49c16abda1e0d34dc600d48e20
dylan-meiners <dylanmeiners@gmail.com>
2019-04-12 22:47:54 -0500

    Fixing static stuff

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    '../scraper',
+    '../../scraper',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
